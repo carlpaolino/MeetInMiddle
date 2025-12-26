35a13960b7d85bdcceb1a5f2f72ece4f88b5ec0c
carlpaolino <carlpaolino05@gmail.com>
2025-12-25 23:07:48 -0500

    xcode

--- MeetInMiddle/ContentView.swift
+++ MeetInMiddle/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world my name is car!")
         }
         .padding()
     }
